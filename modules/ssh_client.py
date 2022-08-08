2619ff5437ab2c959c4141a23e448d84715c47b2
KarolisBanikonis <karolis.banikonis@ktu.edu>
2022-08-08 10:31:18 +0300

    V-1.02, Added SSH, Dynamic load

--- modules/ssh_client.py
+++ modules/ssh_client.py
@@ -1,15 +1,23 @@
 import paramiko
+import time
+
 
 class ConnectionHandler:
 
     __ssh_client = None
+    __ssh_channel = None
 
-    def __init__(self, config):
-        addr = config.get_param("address")
-        username = config.get_param("username")
-        password = config.get_param("password")
-        if not self.__open_connection(addr, username, password):
-            raise Exception("Unable to connect to SSH server")
+    def __init__(self, flags):
+
+        if not flags.ip and not flags.username and not flags.password:
+            raise Exception("fuhas esi")
+
+        print(flags)
+        
+
+        # if not self.__open_connection(addr, username, password):
+        #     raise Exception("Unable to connect to SSH server")
+        
 
     def __close_connection(self):
         if self.__ssh_client:
@@ -35,3 +43,37 @@ class ConnectionHandler:
 
     def __del__(self):
         self.__close_connection()
+
+    def shell_exec_command(self, command, argument):
+        try:
+            self.__ssh_channel = self.__ssh_client.invoke_shell()
+            time.sleep(1)
+            self.__ssh_channel.send(
+                "socat /dev/tty,raw,echo=0,escape=0x03 /dev/ttyUSB3,raw,setsid,sane,echo=0,nonblock ; stty sane\r")
+            time.sleep(1)
+            self.__ssh_channel.send(command + "\r")
+            time.sleep(1)
+            self.__ssh_channel.send(argument + "\r")
+            time.sleep(1)
+            self.__ssh_channel.send("\x1A\r")
+            output = self.__ssh_channel.recv(9999).decode("ascii").splitlines()
+            answer, output = self.wait_until(output, 90, 2)
+            if answer:
+                return output
+            else:
+                return output
+        except Exception as error:
+            print(error)
+
+    def wait_until(self, output, timeout, period=0.25):
+        mustend = time.time() + timeout
+        while time.time() < mustend:
+            if output[-2] == 'OK' or output[-2] == 'FAIL':
+                return True, output[-2]
+            output = self.__ssh_channel.recv(9999).decode("ascii").splitlines()
+            # print(0)
+            if output[-2] == []:
+                continue
+            output = self.__ssh_channel.recv(9999).decode("ascii").splitlines()
+            time.sleep(period)
+        return False, 'Error'
